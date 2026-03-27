99a2a3b4ac9bfa8343996c1ef6dd974baf353aff
Jasdeep Kaur <jasdeepkaur2@rrc.ca>
2026-03-27 03:13:07 -0500

    Add View Store link to ActiveAdmin navigation menu

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,8 +1,15 @@
 ActiveAdmin.setup do |config|
   config.site_title = "Prairie Tech Market Admin"
+  config.site_title_link = "/"
   config.authentication_method = :authenticate_admin_user!
   config.current_user_method = :current_admin_user
   config.logout_link_path = :destroy_user_session_path
   config.logout_link_method = :delete
   config.root_to = "dashboard#index"
+
+  config.namespace :admin do |admin|
+    admin.build_menu do |menu|
+      menu.add label: "View Store", url: "/", priority: 100
+    end
+  end
 end
