e59ebf2033a8e6a07876598029df8c37b7a636d8
Jasdeep Kaur <jasdeepkaur2@rrc.ca>
2026-03-27 02:05:49 -0500

    Add product display pages, category pages, search, home page and Bootstrap layout

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,22 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
+    @products = @products.where(category_id: params[:category_id]) if params[:category_id].present?
+    @products = @products.page(params[:page]).per(12)
+    @categories = Category.all
   end
 
   def show
+    @product = Product.find(params[:id])
+  end
+
+  def search
+    @categories = Category.all
+    @query = params[:query]
+    @category_id = params[:category_id]
+    @products = Product.all
+    @products = @products.where("name LIKE ? OR description LIKE ?", "%#{@query}%", "%#{@query}%") if @query.present?
+    @products = @products.where(category_id: @category_id) if @category_id.present?
+    @products = @products.page(params[:page]).per(12)
   end
 end
