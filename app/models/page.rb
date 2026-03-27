10dbb73ce9cc31cf63f90730a2a11dc550c2a5e8
Jasdeep Kaur <jasdeepkaur2@rrc.ca>
2026-03-27 03:11:20 -0500

    Add ransackable_attributes to Page model and add validations

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,13 @@
 class Page < ApplicationRecord
-  validates :slug, presence: true, uniqueness: true
   validates :title, presence: true
+  validates :slug, presence: true
   validates :body, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["id", "title", "slug", "body", "created_at", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
