4f463f70261fd63412af56217276e2ba40d76600
Jasdeep Kaur <jasdeepkaur2@rrc.ca>
2026-04-07 13:47:24 -0500

    Add full Province admin with editable GST PST HST tax rates

--- app/admin/provinces.rb
+++ app/admin/provinces.rb
@@ -1,18 +1,32 @@
 ActiveAdmin.register Province do
+  permit_params :name, :gst, :pst, :hst
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :gst, :pst, :hst
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :gst, :pst, :hst]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  index do
+    selectable_column
+    id_column
+    column :name
+    column(:gst) { |p| "#{(p.gst * 100).round(1)}%" }
+    column(:pst) { |p| "#{(p.pst * 100).round(1)}%" }
+    column(:hst) { |p| "#{(p.hst * 100).round(1)}%" }
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :gst, label: "GST (e.g. 0.05 for 5%)"
+      f.input :pst, label: "PST (e.g. 0.07 for 7%)"
+      f.input :hst, label: "HST (e.g. 0.15 for 15%)"
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :name
+      row(:gst) { |p| "#{(p.gst * 100).round(1)}%" }
+      row(:pst) { |p| "#{(p.pst * 100).round(1)}%" }
+      row(:hst) { |p| "#{(p.hst * 100).round(1)}%" }
+    end
+  end
 end
