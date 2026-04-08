443b027097b6438f46f7d296af52c3607e1f75d1
Jasdeep Kaur <jasdeepkaur2@rrc.ca>
2026-04-08 03:23:09 -0500

    Fix categories admin with permit_params to allow creating and editing categories

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,18 +1,28 @@
 ActiveAdmin.register Category do
+  permit_params :name, :description
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :description
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :description]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :description
+    column(:products_count) { |c| c.products.count }
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :description
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :name
+      row :description
+      row(:products_count) { |c| c.products.count }
+    end
+  end
 end
